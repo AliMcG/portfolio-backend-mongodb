bc95b4012d13ac726d1fd74a5f114b93b20f8384
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-07-27 21:38:45 +0100

    redeploy

--- routes/projectDataRouter.js
+++ routes/projectDataRouter.js
@@ -10,4 +10,8 @@ router.get("/project-data", getProjectData)
 
 router.post("/project-data", createProjectData)
 
+router.delete("/", (req, res) => {
+  res.json("Nothing to delete")
+})
+
 export default router
