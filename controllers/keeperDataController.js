6f8b89d7b83991c6c5f3b2e93931bc4355022fbc
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-10-03 14:16:52 +0100

    REVERTING to 6f626ac

--- controllers/keeperDataController.js
+++ controllers/keeperDataController.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // , 
 
 export const getKeeperData = async (req, res) => {
-  const {userId} = req.body.userId
+  const {userId} = req.body
   console.log(userId)
   const data = await Keeper.find({userId: userId}).sort({ createdAt: -1 });
   console.log(data);
